03da1fcd6f2019cedf7f4b7316b71a4a99d47215
Caroline S <cahe0002@stud.ek.dk>
2025-11-03 08:32:24 +0100

    changed grid buttons

--- js/instruction.js
+++ js/instruction.js
@@ -20,22 +20,28 @@
 
 // Mund knappen \\
 
-console.log("Test2");
+console.log("Test1");
 
 document.querySelector("#mund").addEventListener("click", sundb);
 
 function sundb() {
-  console.log("test 2");
+  console.log("test 1");
   document.querySelector("#sund").classList.toggle("visible");
 }
 
+// document.querySelector(".info-text").innerHTML = "hej";
+
+// function textbox() {
+//   document.querySelector(".info-text").classList.toggle("visible");
+// }
+
 // Mave knappen \\
 
-console.log("Test3");
+console.log("Test2");
 document.querySelector("#mave").addEventListener("click", maveb);
 
 function maveb() {
-  console.log("test 3");
+  console.log("test 2");
   document.querySelector("#tsunami").classList.toggle("visible");
 }
 
@@ -46,7 +52,7 @@ console.log("Test3");
 document.querySelector("#ben").addEventListener("click", vaegtb);
 
 function vaegtb() {
-  console.log("test 2");
+  console.log("test 3");
   document.querySelector("#vaegt").classList.toggle("visible");
 }
 
@@ -62,4 +68,17 @@ function vaegtb() {
 //   console.log("removesund");
 //   document.querySelector("#sund").classList;
 //   document.querySelector("#sund").classList.remove("remove");
+// }
+
+// console.log("eye");
+// document.querySelector("#mund").addEventListener("click", rotate);
+// function rotate() {
+//   document.querySelector("#pupil").classList.add("rotate");
+//   document
+//     .querySelector("#pupil")
+//     .addEventListener("animationend", removeRotate);
+// }
+
+// function removeRotate() {
+//   document.querySelector("#pupil").classList.remove("rotate");
 // }
