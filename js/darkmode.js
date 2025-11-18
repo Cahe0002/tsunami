c3064b058efec834d68d358a151896833e51ac75
Caroline S <cahe0002@stud.ek.dk>
2025-11-18 15:25:48 +0100

    removed js

--- js/darkmode.js
+++ js/darkmode.js
@@ -7,23 +7,23 @@ function toggleTheme() {
 
 btn.addEventListener("click", toggleTheme);
 
-if (localStorage.getItem("theme") === "dark") {
-  html.classList.add("dark");
-  btn.classList.add("active");
-  btn.textContent = "Darkmode OFF";
-} else {
-  btn.textContent = "Darkmode ON";
-}
+// if (localStorage.getItem("theme") === "dark") {
+//   html.classList.add("dark");
+//   btn.classList.add("active");
+//   btn.textContent = "Darkmode OFF";
+// } else {
+//   btn.textContent = "Darkmode ON";
+// }
 
-function toggleTheme() {
-  html.classList.toggle("dark");
-  btn.classList.toggle("active");
+// function toggleTheme() {
+//   html.classList.toggle("dark");
+//   btn.classList.toggle("active");
 
-  if (html.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-    btn.textContent = "Darkmode OFF";
-  } else {
-    localStorage.setItem("theme", "light");
-    btn.textContent = "Darkmode ON";
-  }
-}
+//   if (html.classList.contains("dark")) {
+//     localStorage.setItem("theme", "dark");
+//     btn.textContent = "Darkmode OFF";
+//   } else {
+//     localStorage.setItem("theme", "light");
+//     btn.textContent = "Darkmode ON";
+//   }
+// }
